a9f13fd80b043edd191450ed1d179e177b8dd95c
kls-dujinwei\dujinwei <1979159182@qq.com>
2022-10-12 10:07:36 +0800

    demo

--- WindowsFormsAppOffice/Form1.Designer.cs
+++ WindowsFormsAppOffice/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace WindowsFormsAppOffice
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 75);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@ namespace WindowsFormsAppOffice
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(219, 74);
+            this.button2.Location = new System.Drawing.Point(112, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
